8327239368d46145d83131cf25f5035a2c1a85cc
franckenric <enricfranck@gmail.com>
2026-04-13 14:46:46 +0300

    Ajout des scripts deploy et base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio-andriamihaja/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
